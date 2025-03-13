06d5ece5a3ebc376ec0691aa3b48dfcb62717835
Ailén <ailenechaguibel@gmail.com>
2025-03-13 19:06:17 +0100

    ejercicio 2, sin terminar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
  // Aquí tu código
+const lista = document.getElementById('lista');
+const agregar = document.getElementById('agregar');
+let topFive = 0;
+
+agregar.addEventListener('click', function() {
+    
+    if (topFive < 5) {
+        const destino = prompt('Tu top 5 para las vacaciones 2025:');
+        
+        if(destino) {
+            const nuevoLugar = document.createElement('li');
+            nuevoLugar.innerText = destino;
+
+            lista.appendChild(nuevoLugar);
+            contador++;
+    }else {
+            console.log('Necesitas vacaciones!');
+      
+    } 
+}
+  
+});
